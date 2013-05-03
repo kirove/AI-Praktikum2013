fce674377a5f25f8e2dbb1e517d75820b92873ca
sk <sk@141.22.85.149>
2013-05-03 16:11:18 +0200

    - Hibernate hinzugefügt(noch keine Mapping Files)

--- src/Main/HES.java
+++ src/Main/HES.java
@@ -4,6 +4,9 @@
  */
 package Main;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author NED
@@ -16,4 +19,57 @@ public class HES {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    
+    private static void saveOrUpdate(Object object) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = session.beginTransaction();
+
+            session.saveOrUpdate(object);
+            // Committing the change in the database.
+            session.flush();
+            transaction.commit();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+            // Rolling back the changes to make the data consistent in case of any failure
+            // in between multiple database write operations.
+            transaction.rollback();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    private static void delete(Object object) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        Transaction transaction = null;
+
+        try {
+
+            transaction = session.beginTransaction();
+
+            session.delete(object);
+            // Committing the change in the database.
+            session.flush();
+            transaction.commit();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+            // Rolling back the changes to make the data consistent in case of any failure
+            // in between multiple database write operations.
+            transaction.rollback();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+    
 }
