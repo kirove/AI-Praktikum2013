d284e671fb7cb0d0ef0e2ce6c417b339bfdc7fac
Niko <>
2013-05-25 14:52:42 +0200

    reserviereProdukt attributen geändert

--- src/HesClient/HesServiceClient.java
+++ src/HesClient/HesServiceClient.java
@@ -3,7 +3,8 @@ package HesClient;
 import Datentypen.AdresseTyp;
 import Datentypen.TelefonNrTyp;
 import java.rmi.*;
-import rmiServer.RmiServerInterface;
+import HESServer.HESServer;
+import HESServer.RmiServerInterface;
 
 /**
  *
@@ -14,17 +15,21 @@ public class HesServiceClient {
     public static void main(String[] args) throws Exception {
 
         // Assign security manager
-        if (System.getSecurityManager() == null) {
-            System.setSecurityManager(new RMISecurityManager());
-        }
+//        if (System.getSecurityManager() == null) {
+//            System.setSecurityManager(new RMISecurityManager());
+//        }
 
         // Call registry for PowerService
-        RmiServerInterface service = (RmiServerInterface) Naming.lookup("rmi://localhost/HesServer");
+        RmiServerInterface service = (RmiServerInterface) Naming.lookup("rmi://localhost/HESServer");
+        RmiServerInterface service2 = (RmiServerInterface) Naming.lookup("rmi://141.22.81.199/HESServer");
+
         AdresseTyp adr1 = new AdresseTyp("Berliner Tors", 5, 20537, "Hamburg", "Germany");
         TelefonNrTyp telNr = new TelefonNrTyp("040", 12345689);
-        System.out.println(service.erstelleKunde("first","last",adr1, telNr).toString());
-        
+        System.out.println(service.erstelleKunde("first", "last", adr1, telNr).toString());
 
+        AdresseTyp adr2 = new AdresseTyp("Berliner Tors", 5, 20537, "Hamburg", "Germany");
+        TelefonNrTyp telNr2 = new TelefonNrTyp("040", 12345689);
+        System.out.println(service2.erstelleKunde("first2", "last2", adr2, telNr2).toString());
 
     }
 }
