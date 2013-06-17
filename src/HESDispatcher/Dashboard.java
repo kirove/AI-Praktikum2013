ca2bc31d2b18a7a7dad219c557baccb117002af1
NED <NED@141.22.80.13>
2013-06-18 00:47:42 +0200

    with the delete commit and some Monitor update

--- src/HESDispatcher/Dashboard.java
+++ src/HESDispatcher/Dashboard.java
@@ -1,8 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package HESClient;
+
+package HESDispatcher;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
